b7adf32cf0e039c3e0b0fa544b1b7769cfc4abe6
Swapnil Magar <magarswapnil896@gmail.com>
2020-10-08 19:22:44 +0530

    Initial Commit

---
+++ MarvelAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelAPI
+//
+//  Created by Swapnil  Magar on 08/10/20.
+//  Copyright © 2020 Swapnil Magar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
